d410d6ce2b432e90144ad78f53fda653fa50d22a
kartik <kabakshi@hotmail.ca>
2021-11-03 23:56:35 -0400

    fetcher

--- fetcher.js
+++ fetcher.js
@@ -6,12 +6,14 @@ let URL = process.argv[2];
 let localPath = process.argv[3];
 
 request(URL, function(error, response, body) {
- 
-  fs.writeFile(localPath, body, function(error) {
+  if (error){
+    return error;
+  }
+ fs.writeFile(localPath, body, function(error) {
     if (error) {
-      console.log("Can't write to file");
-      return;
-    } else 
-    console.log(`Downloaded and saved ${response} bytes to ${localPath}`);
-  });
-});
+      return ("Can't write to file");
+    }
+  
+    console.log(`Downloaded and saved ${response['headers']['content-length']} bytes to ${localPath}`);
+  })
+})
